f7b3813864e9b28bab4e875c675a2d6e909dc791
rodolphorodriguesjp <115420769+rodolphorodriguesjp@users.noreply.github.com>
2022-11-22 23:11:00 -0300

    Add files via upload

---
+++ Rodolpho.js
@@ -0,0 +1,156 @@
+class Aluno {
+
+    nome;
+    matricula;
+    nota1;
+    nota2;
+    nota3;
+    media;
+  
+    constructor(nome, matricula, nota1, nota2, nota3) {
+  
+       this.nome = nome;
+       this.matricula = matricula;
+       this.nota1 = nota1;
+       this.nota2 = nota2;
+       this.nota3 = nota3;
+       this.media = this.calculaMedia(nota1, nota2, nota3);
+       
+       this.arrayAlunos = [];
+  
+    }
+
+    adicionar(aluno) {
+      this.arrayAlunos.push(aluno);
+    }
+    
+    salvar(){
+      let aluno = this.lerDados();
+
+      if(this.validaCampos(aluno) == true){
+        this.adicionar(aluno);
+        
+      }
+
+      this.listaTabela();
+      this.limpar();
+    }
+
+    listaTabela(){
+      let tbody = document.querySelector('#tbody');
+      tbody.innerText = '';
+
+      for(let i = 0; i < this.arrayAlunos.length; i++){
+        let tr = tbody.insertRow();
+        let nome = tr.insertCell();
+        let matricula = tr.insertCell();
+        let nota1 = tr.insertCell();
+        let nota2 = tr.insertCell();
+        let nota3 = tr.insertCell();
+        let media = tr.insertCell();
+        let opcoes = tr.insertCell();
+    
+        nome.innerText = this.arrayAlunos[i].nome;
+        matricula.innerText = this.arrayAlunos[i].matricula;
+        nota1.innerText = this.arrayAlunos[i].nota1;
+        nota2.innerText = this.arrayAlunos[i].nota2;
+        nota3.innerText = this.arrayAlunos[i].nota3;
+        media.innerText = this.arrayAlunos[i].media;
+
+        matricula.classList.add('center');
+        nome.classList.add('center');
+        nota1.classList.add('center');
+        nota2.classList.add('center');
+        nota3.classList.add('center');
+        media.classList.add('center');
+        
+
+        let imgDelete = document.createElement('i');
+        imgDelete.classList.add('bi');
+        imgDelete.classList.add('bi-trash')
+        imgDelete.setAttribute("onclick","aluno.deletar("+this.arrayAlunos[i].matricula+")");
+
+        opcoes.appendChild(imgDelete);
+        opcoes.classList.add('center');
+
+      }
+    }
+
+    lerDados() {
+      let aluno = {}
+      
+      aluno.nome = document.querySelector('#nome').value;
+      aluno.matricula = document.querySelector('#matricula').value;
+      aluno.nota1 = parseFloat(document.querySelector('#nota1').value);
+      aluno.nota2 = parseFloat(document.querySelector('#nota2').value);
+      aluno.nota3 = parseFloat(document.querySelector('#nota3').value);
+      aluno.media = this.calculaMedia(aluno.nota1, aluno.nota2, aluno.nota3);
+
+      return aluno;
+    }
+
+    validaCampos(aluno) {
+      let msg = '';
+
+      if(aluno.nome == ''){
+        msg += ' - Insira o nome \n';
+      }
+      if(aluno.matricula == ''){
+        msg += ' - Insira a matrícula \n';
+      }
+      if(aluno.nota1 == ''){
+        msg += ' - Insira primeira avaliação \n';
+      }
+      if(aluno.nota2 == ''){
+        msg += ' - Insira segunda avaliação \n';
+      }
+      if(aluno.nota3 == ''){
+        msg += ' - Insira terceira avaliaçãoo \n';
+      }
+      if(msg != ''){
+        alert(msg);
+        return false;
+      }
+
+      for(let i = 0; i < this.arrayAlunos.length; i++){
+        if (aluno.matricula == this.arrayAlunos[i].matricula){
+          alert('Matricula já foi adicionada ao diário de classe');
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    limpar(){      
+      document.querySelector('#nome').value = '';
+      document.querySelector('#matricula').value = '';
+      document.querySelector('#nota1').value = '';
+      document.querySelector('#nota2').value = '';
+      document.querySelector('#nota3').value = '';
+      
+    }
+    
+    deletar(matricula){
+
+      if(confirm('Excluir notas avaliativas do aluno '+ matricula +' ?')){
+
+      let tbody = document.querySelector('#tbody');
+
+      for(let i = 0; i < this.arrayAlunos.length; i++){
+        if (this.arrayAlunos[i].matricula == matricula){
+          this.arrayAlunos.splice(i,1);
+          tbody.deleteRow(i);
+        }
+      }
+
+      console.log(this.arrayAlunos)
+      }
+    }
+    calculaMedia(nota1, nota2, nota3){
+      return parseFloat(((nota1 + nota2 + nota3) / 3).toFixed(2));
+    }
+  }
+  var aluno = new Aluno();
+
+
